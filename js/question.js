ca1809af75039cac99abd09355ed10c6d3d90c5c
Md-Majabul-Islam <134044820+Md-Majabul-Islam@users.noreply.github.com>
2023-07-11 17:45:46 +0600

    added

--- js/question.js
+++ js/question.js
@@ -56,4 +56,305 @@ let questions = [
           "eXamine Multiple Language"
         ]
       },
+
+      {
+        numb: 6,
+        question: "What does DB stand for?",
+        answer: "Database",
+        options: [
+          "Database Management System",
+          "eXecutable Multiple Language",
+          "eXTra Multi-Program Language",
+          "Database"
+        ]
+      },
+
+      {
+        numb: 7,
+        question: "What does DBMS stand for?",
+        answer: "Database Management System",
+        options: [
+          "Database Management System",
+          "eXecutable Multiple Language",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+
+      {
+        numb: 8,
+        question: "What does HTTP stand for?",
+        answer: "Hypertext Transfer Protocol",
+        options: [
+          "Database Management System",
+          "Hypertext Transfer Protocol",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+
+      {
+        numb: 9,
+        question: "What does WWW stand for?",
+        answer: "World Wide Web",
+        options: [
+          "Database Management System",
+          "Hypertext Transfer Protocol",
+          "eXTra Multi-Program Language",
+          "World Wide Web"
+        ]
+      },
+
+      {
+        numb: 10,
+        question: "What does WMV stand for?",
+        answer: "Windows Media Video",
+        options: [
+          "Database Management System",
+          "Hypertext Transfer Protocol",
+          "Windows Media Video",
+          "eXamine Multiple Language"
+        ]
+      },
+
+      {
+        numb: 11,
+        question: "What does PAN stand for?",
+        answer: "Personal Area Network",
+        options: [
+          "Database Management System",
+          "Personal Area Network",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+
+      {
+        numb: 12,
+        question: "What does LAN stand for?",
+        answer: "Local Area Network",
+        options: [
+          "Database Management System",
+          "Hypertext Transfer Protocol",
+          "Local Area Network",
+          "eXamine Multiple Language"
+        ]
+      },
+
+      {
+        numb: 13,
+        question: "What does MAN stand for?",
+        answer: "Metropolitan Area Network",
+        options: [
+          "Database Management System",
+          "Hypertext Transfer Protocol",
+          "Local Area Network",
+          "Metropolitan Area Network"
+        ]
+      },
+
+      {
+        numb: 14,
+        question: "What does WAN stand for?",
+        answer: "Wide Area Network",
+        options: [
+          "Wide Area Network",
+          "Hypertext Transfer Protocol",
+          "Local Area Network",
+          "Metropolitan Area Network"
+        ]
+      },
+
+      {
+        numb: 15,
+        question: "What does URL stand for?",
+        answer: "Uniform Resource Locator",
+        options: [
+          "Uniform Resource Locator",
+          "Hypertext Transfer Protocol",
+          "Local Area Network",
+          "Metropolitan Area Network"
+        ]
+      },
+
+      
+      {
+        numb:16,
+        question:'What Does URI Stand For?',
+        answer:'Uniform Resource Identifier',
+        options:[
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Uniform Resource Identifier",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 17,
+        question: "What Does CSS Stand For?",
+        answer: "Universal Serial Bus",
+        options: [
+          "Universal Serial Bus",
+          "Colorful Style Sheet",
+          "Computer Style Sheet",
+          "Cascading Style Sheet"
+        ]
+      },
+      {
+        numb: 18,
+        question: "What Does UML Stand For?",
+        answer: "Unified Modeling Language",
+        options: [
+          "Hypertext Preprocessor",
+          "Hypertext Programming",
+          "Unified Modeling Language",
+          "Hometext Preprocessor"
+        ]
+      },
+    
+      {
+        numb: 19,
+        question: "What does UI stand for?",
+        answer: "User Interface",
+        options: [
+          "User Interface",
+          "Stylesheet Query Language",
+          "Statement Question Language",
+          "Structured Query Language"
+        ]
+      },
+
+      {
+        numb: 20,
+        question: "What does WLAN stand for?",
+        answer: "Wireless Local-Area Network",
+        options: [
+          "eXtensible Markup Language",
+          "Wireless Local-Area Network",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+
+      {
+        numb: 21,
+        question: "What does WMA stand for?",
+        answer: "Windows Media Audio",
+        options: [
+          "Database Management System",
+          "Hypertext Transfer Protocol",
+          "Windows Media Video",
+          "Windows Media Audio"
+        ]
+      },
+
+      {
+        numb: 22,
+        question: "What does TB stand for?",
+        answer: "Tera Byte",
+        options: [
+          "Database Management System",
+          "Worldwide Interoperability for Microwave Access",
+          "eXTra Multi-Program Language",
+          "Tera Byte"
+        ]
+      },
+
+      {
+        numb: 23,
+        question: "What does Wi-Fi stand for?",
+        answer: "wireless fidelity",
+        options: [
+          "Database Management System",
+          "Hypertext Transfer Protocol",
+          "eXTra Multi-Program Language",
+          "Wireless Fidelity"
+        ]
+      },
+
+      {
+        numb: 24,
+        question: "What does WAP stand for?",
+        answer: "Wireless Access Point",
+        options: [
+          "Wireless Protocol",
+          "Wireless Access Point",
+          "World Wide Web",
+          "Wireless Application"
+        ]
+      },
+
+      {
+        numb: 25,
+        question: "What does VPN stand for?",
+        answer: "Virtual Private Network",
+        options: [
+          "Virtual Private Network",
+          "Hypertext Transfer Protocol",
+          "Windows Media Video",
+          "eXamine Multiple Language"
+        ]
+      },
+
+      {
+        numb: 26,
+        question: "What does UPS stand for?",
+        answer: "Uninterruptible Power Supply",
+        options: [
+          "Uninterruptible Power Supply",
+          "Personal Area Network",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+
+      {
+        numb: 27,
+        question: "What does TTL stand for?",
+        answer: "Transistor-Transistor Logic",
+        options: [
+          "Database Management System",
+          "Hypertext Transfer Protocol",
+          "Transistor-Transistor Logic",
+          "eXamine Multiple Language"
+        ]
+      },
+
+      {
+        numb: 28,
+        question: "What does TSP stand for?",
+        answer: "Travelling salesman problem",
+        options: [
+          "Database Management System",
+          "Hypertext Transfer Protocol",
+          "Travelling salesman problem",
+          "Metropolitan Area Network"
+        ]
+      },
+
+      {
+        numb: 29,
+        question: "What does TCP stand for?",
+        answer: "Transmission Control Protocol",
+        options: [
+          "Transmission Control Protocol",
+          "Hypertext Transfer Protocol",
+          "Local Area Network",
+          "Metropolitan Area Network"
+        ]
+      },
+
+      {
+        numb: 30,
+        question: "What does IP stand for?",
+        answer: "Internet Protocol",
+        options: [
+          "Uniform Resource Locator",
+          "Hypertext Transfer Protocol",
+          "Local Area Network",
+          "Internet Protocol"
+        ]
+      },
+
+
 ]
--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
-const MyBtn = document.querySelector(".MyBtn button"); 
+const MyBtn = document.querySelector(".MyBtn button");
 const RulesBox = document.querySelector(".RulesBox");
 const exitButton = document.querySelector(".Buttons .ExitButton");
 const ContinueButton = document.querySelector(".Buttons .ContinueButton");
 const Questions = document.querySelector(".Questions")
-const option_list = document.querySelector(".MyOptions"); 
-const timeCount = document.querySelector(".TimeCount .Seconds"); 
-const timeLine = document.querySelector(".QuestionsHeader .time_lines"); 
-const timeOff = document.querySelector(".QuestionsHeader .TimeLeft"); 
+const option_list = document.querySelector(".MyOptions");
+const timeCount = document.querySelector(".TimeCount .Seconds");
+const timeLine = document.querySelector(".QuestionsHeader .time_lines");
+const timeOff = document.querySelector(".QuestionsHeader .TimeLeft");
 
 
 
- MyBtn.onclick = ()=>{
-    RulesBox.classList.add("activeInfo"); 
-    }
-    
-exitButton.onclick = ()=>{
-        RulesBox.classList.remove("activeInfo"); 
+MyBtn.onclick = () => {
+    RulesBox.classList.add("activeInfo");
+}
+
+exitButton.onclick = () => {
+    RulesBox.classList.remove("activeInfo");
 }
 
 
-ContinueButton.onclick = ()=>{
-    RulesBox.classList.remove("activeInfo"); 
-    Questions.classList.add("activeQuiz"); 
+ContinueButton.onclick = () => {
+    RulesBox.classList.remove("activeInfo");
+    Questions.classList.add("activeQuiz");
     showQuestions(0);
     startTimer(15)
 
-    startTimerLine(0); 
+    startTimerLine(0);
 
 }
 
@@ -37,193 +37,193 @@ const reslut_box = document.querySelector(".reslut_box");
 const restart_quiz = document.querySelector(".buttons .restart1");
 const quit_quiz = document.querySelector(".buttons .quit");
 
-restart_quiz.onclick = ()=>{
-    Questions.classList.add("activeQuiz"); 
-    reslut_box.classList.remove("activeResult"); 
-    let que_count = 0; 
-    let timeValue = 15; 
+restart_quiz.onclick = () => {
+    Questions.classList.add("activeQuiz");
+    reslut_box.classList.remove("activeResult");
+    let que_count = 0;
+    let timeValue = 15;
 
     let widthValue = 0;
-    let userScore =0; 
+    let userScore = 0;
     showQuestions(que_count);
-    clearInterval(counter); 
+    clearInterval(counter);
     startTimer(timeValue)
-    clearInterval(counterLine); 
-    startTimerLine(widthValue); 
-    nextBtn.style.display = "none"; 
+    clearInterval(counterLine);
+    startTimerLine(widthValue);
+    nextBtn.style.display = "none";
     timeOff.textContent = "Time Left";
 
 }
 
 
-quit_quiz.onclick = ()=>{
+quit_quiz.onclick = () => {
     window.location.reload();
 }
 
-let que_count = 0; 
-let counter; 
-let timeValue = 15; 
+let que_count = 0;
+let counter;
+let timeValue = 15;
 
-let counterLine; 
+let counterLine;
 let widthValue = 0;
-let userScore =0; 
+let userScore = 0;
 
-nextBtn.onclick = ()=> {
-    if(que_count  <questions.length - 1 ){
-        que_count ++ 
+nextBtn.onclick = () => {
+    if (que_count < questions.length - 1) {
+        que_count++
         showQuestions(que_count);
-        clearInterval(counter); 
+        clearInterval(counter);
         startTimer(timeValue)
 
-        clearInterval(counterLine); 
-        startTimerLine(widthValue); 
-        nextBtn.style.display = "none"; 
+        clearInterval(counterLine);
+        startTimerLine(widthValue);
+        nextBtn.style.display = "none";
         timeOff.textContent = "Time Left";
 
-    }else{
-        clearInterval(counter); 
-        clearInterval(counterLine); 
+    } else {
+        clearInterval(counter);
+        clearInterval(counterLine);
         console.log("You Have Completd Your Task 🥰");
         showResultBox()
-       
+
     }
 }
 
 function showQuestions(index) {
- const que_text = document.querySelector('.text');
+    const que_text = document.querySelector('.text');
 
- let option_tag =  '<div class="options"><span>' + questions[index].options[0]  +'</span></div>'
-                    + '<div class="options"><span>' + questions[index].options[1]  +'</span></div>'
-                    + '<div class="options"><span>' + questions[index].options[2]  +'</span></div>'
-                    + '<div class="options"><span>' + questions[index].options[3]  +'</span></div>'
-
-
- let que_tag = "<span>" + questions[index].numb+ "." + questions[index].question + " </span>"; 
- que_text.innerHTML = que_tag;
-option_list.innerHTML = option_tag;
-const total_que = document.querySelector(".total_que");
-let total_queTag  = '<p>' + questions[index].numb + ' Of 5 </p>'
-total_que.innerHTML = total_queTag;
+    let option_tag = '<div class="options"><span>' + questions[index].options[0] + '</span></div>'
+        + '<div class="options"><span>' + questions[index].options[1] + '</span></div>'
+        + '<div class="options"><span>' + questions[index].options[2] + '</span></div>'
+        + '<div class="options"><span>' + questions[index].options[3] + '</span></div>'
 
 
-
-const option = option_list.querySelectorAll(".options"); 
-for(let i=0; i<option.length; i++){
-    option[i].setAttribute("onclick", "optionSelected(this)"); 
-}
-
-}
-
-let tickIcon ='<div class="tick icon"><i class="fas fa-check"></i></div>'; 
-let crossIcon = '<div class="cross icon"><i class="fas fa-times"></i></div>'; 
-
-
-function optionSelected(answer){
-    clearInterval(counter); 
-    clearInterval(counterLine); 
-    let userAns = answer.textContent; 
-    let correctAns = questions[que_count].answer; 
-   let alloptions = option_list.children.length;  
-
-   if(userAns == correctAns){
-       userScore +=1 ;
-       console.log(userScore); 
-      answer.classList.add("correct")
-       console.log("Answer Is Correct"); 
-     answer.insertAdjacentHTML("beforeend", tickIcon); 
-   }else{
-      answer.classList.add("incorrect")
-       console.log("Answer Is Wrong ");
-        answer.insertAdjacentHTML("beforeend", crossIcon); 
-
-       for(let i =0; i<alloptions; i++){
-           if(option_list.children[i].textContent == correctAns ){
-            option_list.children[i].setAttribute("class", "options correct"); 
-            option_list.children[i].insertAdjacentHTML("beforeend", tickIcon); 
-           }
-       }
-   }
+    let que_tag = "<span>" + questions[index].numb + "." + questions[index].question + " </span>";
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+    const total_que = document.querySelector(".total_que");
+    let total_queTag = '<p>' + questions[index].numb + ' Of 30 </p>'
+    total_que.innerHTML = total_queTag;
 
 
 
-
-for(let i=0; i<alloptions; i++){
-option_list.children[i].classList.add("disabled"); 
-}
-
-nextBtn.style.display = "block"; 
-
-
-}
-
-
-function showResultBox(){
-    RulesBox.classList.remove("activeInfo"); 
-    Questions.classList.remove("activeQuiz"); 
-    reslut_box.classList.add("activeResult");
-    
-    const scoreText = document.querySelector(".score_text"); 
-    if(userScore > 3){
-       let scoreTag = '<span>Congratulations You Got <p>'+ userScore +'</p> Out Of <p>'+questions.length +'</p></span>';
-        scoreText.innerHTML = scoreTag; 
+    const option = option_list.querySelectorAll(".options");
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
     }
-   else if(userScore > 1){
-        let scoreTag = '<span>Carry On 👌 You Got <p>'+ userScore +'</p> Out Of <p>'+questions.length +'</p></span>';
-         scoreText.innerHTML = scoreTag; 
-     }
-     else{
-        let scoreTag ='<span> I Am  Sorry You Got <p>'+ userScore +'</p> Out Of <p>'+ questions.length +'</p></span>'; 
-    
-        scoreText.innerHTML = scoreTag;
-    }
-   
+
 }
 
+let tickIcon = '<div class="tick icon"><i class="fas fa-check"></i></div>';
+let crossIcon = '<div class="cross icon"><i class="fas fa-times"></i></div>';
 
 
-function startTimer(time){
-    counter = setInterval(timer, 1000 ); 
-   function timer(){
-    timeCount.textContent = time;
-    time--; 
+function optionSelected(answer) {
+    clearInterval(counter);
+    clearInterval(counterLine);
+    let userAns = answer.textContent;
+    let correctAns = questions[que_count].answer;
+    let alloptions = option_list.children.length;
 
-    if(time <9){
-        let addZero = timeCount.textContent; 
-        timeCount.textContent = "0" + addZero
-    }
-    if(time <0){
-        clearInterval(counter)
-        timeCount.textContent ="00"; 
-        timeOff.textContent = "Time Off";
-        let correctAns = questions[que_count].answer; 
-        let alloptions = option_list.children.length;  
-        for(let i =0; i<alloptions; i++){
-            if(option_list.children[i].textContent == correctAns ){
-             option_list.children[i].setAttribute("class", "options correct"); 
-             option_list.children[i].insertAdjacentHTML("beforeend", tickIcon); 
+    if (userAns == correctAns) {
+        userScore += 1;
+        console.log(userScore);
+        answer.classList.add("correct")
+        console.log("Answer Is Correct");
+        answer.insertAdjacentHTML("beforeend", tickIcon);
+    } else {
+        answer.classList.add("incorrect")
+        console.log("Answer Is Wrong ");
+        answer.insertAdjacentHTML("beforeend", crossIcon);
+
+        for (let i = 0; i < alloptions; i++) {
+            if (option_list.children[i].textContent == correctAns) {
+                option_list.children[i].setAttribute("class", "options correct");
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
             }
         }
-        
-        for(let i=0; i<alloptions; i++){
-            option_list.children[i].classList.add("disabled"); 
-            }
-            
-            nextBtn.style.display = "block"; 
-
     }
-    
-   }
+
+
+
+
+    for (let i = 0; i < alloptions; i++) {
+        option_list.children[i].classList.add("disabled");
+    }
+
+    nextBtn.style.display = "block";
+
 
 }
 
 
-function startTimerLine(time){
+function showResultBox() {
+    RulesBox.classList.remove("activeInfo");
+    Questions.classList.remove("activeQuiz");
+    reslut_box.classList.add("activeResult");
+
+    const scoreText = document.querySelector(".score_text");
+    if (userScore > 3) {
+        let scoreTag = '<span>Congratulations You Got <p>' + userScore + '</p> Out Of <p>' + questions.length + '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else if (userScore > 1) {
+        let scoreTag = '<span>Carry On 👌 You Got <p>' + userScore + '</p> Out Of <p>' + questions.length + '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else {
+        let scoreTag = '<span> I Am  Sorry You Got <p>' + userScore + '</p> Out Of <p>' + questions.length + '</p></span>';
+
+        scoreText.innerHTML = scoreTag;
+    }
+
+}
+
+
+
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+    function timer() {
+        timeCount.textContent = time;
+        time--;
+
+        if (time < 9) {
+            let addZero = timeCount.textContent;
+            timeCount.textContent = "0" + addZero
+        }
+        if (time < 0) {
+            clearInterval(counter)
+            timeCount.textContent = "00";
+            timeOff.textContent = "Time Off";
+            let correctAns = questions[que_count].answer;
+            let alloptions = option_list.children.length;
+            for (let i = 0; i < alloptions; i++) {
+                if (option_list.children[i].textContent == correctAns) {
+                    option_list.children[i].setAttribute("class", "options correct");
+                    option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
+                }
+            }
+
+            for (let i = 0; i < alloptions; i++) {
+                option_list.children[i].classList.add("disabled");
+            }
+
+            nextBtn.style.display = "block";
+
+        }
+
+    }
+
+}
+
+
+function startTimerLine(time) {
     counterLine = setInterval(timer, 50);
-    function timer(){
-        time += 1 ; 
-        timeLine.style.width = time + "px"; 
-        if(time > 319){
-            clearInterval(counterLine); 
+    function timer() {
+        time += 1;
+        timeLine.style.width = time + "px";
+        if (time > 319) {
+            clearInterval(counterLine);
         }
     }
 }
